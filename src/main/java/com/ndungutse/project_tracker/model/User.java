f36996ce641303995f78ba64cd6f5029a0b604a3
ericndungutse <dav.ndungutse@gmail.com>
2025-06-15 00:02:22 +0200

    feat: create user model and and user repository

--- src/main/java/com/ndungutse/project_tracker/model/User.java
+++ src/main/java/com/ndungutse/project_tracker/model/User.java
@@ -1,4 +1,34 @@
 package com.ndungutse.project_tracker.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    @ToString.Exclude
+    private Role role;
 }
